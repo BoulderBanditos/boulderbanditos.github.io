f0fae7897ee95271c8dadaf84183750bdb271849
William Kanter <will@Williams-MacBook-Pro.local>
2020-04-13 18:01:12 -0600

    fixed app listen

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser'); //Ensure our body-parser tool has been 
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// var pgp = require('pg-promise')();
+var pgp = require('pg-promise')();
 
 // const dbConfig = {
 // 	host: 'localhost',
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // 	password: 'pwd'
 // };
 
-// const dbConfig = process.env.DATABASE_URL;
-//
-// var db = pgp(dbConfig);
+const dbConfig = process.env.DATABASE_URL;
+
+var db = pgp(dbConfig);
 
 // view engine
 app.set('view engine', 'ejs');
@@ -42,7 +42,10 @@ app.get('/game', function(req, res){
   });
 });
 
+// ........blog?session_key="sessionkey u sent when logged in"
 app.get('/blog', function(req,res){
+var session =req.query.session_key;
+//make a db query toc echk if the session is there
   res.render('pages/blog', {
     title: "Blog"
   });
@@ -50,14 +53,32 @@ app.get('/blog', function(req,res){
 
 app.get('/login', function(req,res){
   //var user =
+  var sessionKey = randomNum;
+
   res.render('pages/login', {
     title: "Login",
     local_css: "login.css"
   });
 });
 
+app.post('/login', function(req, res) {
+//logic to cvalidate username
+var userne;
+console.log(request.body.username);
+console.log(request.body.password);
+
+//db quey to check if they are cvalidate
+
+//sucess
+var sessionkey = "randomly generated string";
+//stre sesssion key against the user in the table
+//redirect orrender the user /home?sessionkey=sessionke
 
 
-app.listen(3000);
+
+});
+
+
+//app.listen(3000);
 // app.listen(process.env.PORT);
 console.log('3000 is the magic port');
